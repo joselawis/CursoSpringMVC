865392e2b388acc41a67f12c0981bc3d5233ad23
unknown <Administrador@ALC-JQ4MM12.usersad.everis.int>
2019-10-14 15:36:21 +0200

    Metodo delete y Put

--- curso-spring-mvc/src/main/java/com/everis/alicante/training/spring/mvc/controllers/PersonasRestController.java
+++ curso-spring-mvc/src/main/java/com/everis/alicante/training/spring/mvc/controllers/PersonasRestController.java
@@ -3,9 +3,12 @@ package com.everis.alicante.training.spring.mvc.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -26,7 +29,7 @@ public class PersonasRestController {
 	
 	@GetMapping("/{id}")
 	public Persona getById(@PathVariable String id){
-		return (Persona) listaPersonas.stream().filter(persona-> persona.getId().equals(id)).findAny().orElse(null);
+		return (Persona) listaPersonas.stream().filter(p-> p.getId().equals(id)).findAny().orElse(null);
 	}
 
 	@PostMapping
@@ -35,4 +38,34 @@ public class PersonasRestController {
 		return listaPersonas;
 	}
 
+	@DeleteMapping("/{id}")
+	public List<Persona> delete(@PathVariable String id){
+		listaPersonas.removeIf(p -> p.getId().equals(id));
+		return listaPersonas;
+	}
+	
+	/*@PutMapping("/{id}")
+	public Persona modificar(@PathVariable String id, @RequestBody Persona persona) {
+		if(!id.equals(persona.getId())) {
+			throw new IllegalArgumentException("Los ids no coinciden");
+		}
+		return listaPersonas.stream().filter(p-> p.getId().equals(id)).peek(p-> {
+			p.setNombre(persona.getNombre());
+			p.setEdad(persona.getEdad());
+		}).findAny().orElse(null);
+	}*/
+	
+	@PutMapping("/{id}")
+	public ResponseEntity<Persona> modificar(@PathVariable String id, @RequestBody Persona persona) {
+		if(!id.equals(persona.getId())) {
+			return ResponseEntity.badRequest().header("error","Los ids no coinciden").build();
+		}
+		final Persona resultado = listaPersonas.stream().filter(p-> p.getId().equals(id)).peek(p-> {
+			p.setNombre(persona.getNombre());
+			p.setEdad(persona.getEdad());
+		}).findAny().orElse(null);
+		
+		return ResponseEntity.ok(resultado);
+	}
+	
 }
